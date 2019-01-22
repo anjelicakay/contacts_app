9c2bf8f8df913dc316763742ad67efac5e4ba0fb
anjelicakay <aktowner@gmail.com>
2019-01-21 22:48:58 -0600

    jbuilder partial

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,14 @@
 class Contact < ApplicationRecord
+  def friendly_updated_at
+    created_at.strftime("%b %d, %Y")
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def japan_phone_number
+    "+81 #{phone_number}"
+  end  
+
 end
