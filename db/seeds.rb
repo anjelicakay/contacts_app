43e264b3a79b042687239818802244f37c2d6431
anjelicakay <aktowner@gmail.com>
2019-02-01 12:41:07 -0600

    ContactGroup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,37 @@
 
-1000.times do
+# 1000.times do
 
-  first_name = Faker::Name.first_name 
-  last_name = Faker::Name.last_name
-  contact = Contact.new(
-                        first_name: first_name,
-                        last_name: last_name,
-                        email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
-                        phone_number: Faker::PhoneNumber.phone_number
-                        )
+#   first_name = Faker::Name.first_name 
+#   last_name = Faker::Name.last_name
+#   contact = Contact.new(
+#                         first_name: first_name,
+#                         last_name: last_name,
+#                         email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
+#                         phone_number: Faker::PhoneNumber.phone_number
+#                         )
 
 
-  contact.save
-end
+#   contact.save
+# end
+
+# Group.create([
+#   {name: "Family"},
+#   {name: "Work"},
+#   {name: "Friends"}
+# ])
+
+# ContactGroup.create([
+#   {contact_id: 1, group_id: 1},
+#   {contact_id: 2, group_id: 1},
+#   {contact_id: 2, group_id: 2},
+#   {contact_id: 3, group_id: 2},
+#   {contact_id: 4, group_id: 2},
+#   {contact_id: 5, group_id: 2},
+#   {contact_id: 5, group_id: 3},
+#   {contact_id: 6, group_id: 2},
+#   {contact_id: 6, group_id: 3},
+#   {contact_id: 8, group_id: 3},
+#   {contact_id: 9, group_id: 3},
+#   {contact_id: 10, group_id: 3}
+# ])
+
